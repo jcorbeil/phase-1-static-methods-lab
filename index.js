20b77b05691831c334e34099dfc7fc985aadab3b
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-27 20:16:20 -0400

    finished-phase=1-static-methods-lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9\s'-]/g, '');
+  }
+
+  static titleize(str) {
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word)) {
+        return Formatter.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(' ');
+  }
 }
